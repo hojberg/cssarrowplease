636e45e02c2db2f4626c61cc9dd0752592aa52ee
Simon Højberg <r.hackr@gmail.com>
2014-08-02 10:26:10 -0700

    simplify deploymeny and serving

---
+++ server.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var connect = require('connect');
+var http = require('http');
+var port = process.env.PORT || 3000;
+var static = connect.static('public');
+
+console.log('CSSArrowPlease on http://localhost:' + port);
+
+http.createServer( connect().use( static ) ).listen( port );

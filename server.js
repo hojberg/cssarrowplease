1329f1333af1e744f2d4724c9bef055392fea2a7
Simon Højberg <r.hackr@gmail.com>
2020-09-16 11:32:39 -0400

    clean up and remove server preparing for netlify

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var http = require('http');
-var port = process.env.PORT || 3000;
-var static = connect.static('public');
-
-console.log('CSSArrowPlease on http://localhost:' + port);
-
-http.createServer( connect().use( static ) ).listen( port );
